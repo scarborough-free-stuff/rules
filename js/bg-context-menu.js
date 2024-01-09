d20fb391c3d5b8018b82fcc81e2ff83e8ec539f9
theresayuy <ty.pasta9066+gh1@proton.me>
2024-01-09 15:57:35 -0500

    update - turned this static website into a url shortcut thing

--- js/bg-context-menu.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", () => {
-    const bgDiv = document.querySelector("#bg");
-    bgDiv.oncontextmenu = () => {
-        return false;
-    }
-})
---
+++ route.js
@@ -0,0 +1,3 @@
+window.onload = () => {
+    window.open("https://www.facebook.com/groups/524212175214038/permalink/1159857094982873", "_self");
+}
