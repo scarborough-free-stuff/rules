1ad5f01b88e31f425029047e089990c82bad995f
theresayuy <ty.pasta9066+gh1@proton.me>
2024-01-09 01:37:39 -0500

    update: removed console.log from sticky-nav.js

--- js/sticky-nav.js
+++ js/sticky-nav.js
@@ -1,10 +1,8 @@
 function stickyNav() {
     let navbar = document.querySelector("#root header nav");
     if (window.pageYOffset > 0) {
-        console.log("navbar is sticky");
         navbar.classList.add("sticky");
     } else {
-        console.log("navbar is not sticky");
         navbar.classList.remove("sticky");
     }
 }
